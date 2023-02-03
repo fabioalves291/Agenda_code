c8ca526c7f7f754e7f03ad17c7ec2004bd357134
jose fabio <fabioalvesdeandrade29@gmai.com>
2023-02-03 16:46:36 -0300

    autosave

--- controllers/default.py
+++ controllers/default.py
@@ -1,1 +1,13 @@
-def adicionarponto(mateira,tempodeestudo):
+
+import tarefas
+def adicionartempo(materia,tempodeestudo):
+    print(tarefas)
+    print(materia)
+    print(tempodeestudo)
+def adicionarpositionmateria():
+    pass
+def adicionarposition():
+    pass
+
+
+
--- tarefas.py
+++ tarefas.py
@@ -1,20 +1,23 @@
 from datetime import datetime
-from controllers import *
+from controllers.default import *
 from controllers.tarefas.tarefas import *
+
 
 if __name__ == "__main__":
     while True:
         for materiageral in tarefas:
             for materia in materiageral:
-                datainicial = datetime.now()
+                datainicial     = datetime.now()
                 print(materia,"iniciou às",datainicial.time())
                 input("Enter quando terminar:")
-                datafinal  = datetime.now()
+                datafinal       = datetime.now()
                 print("Finalizado",materia,"às",datafinal.time())
-                tempodeestudo   =   datafinal-datainicial
-                adicionarponto(materia,tempodeestudo)
+                tempodeestudo   =   datafinal - datainicial
+                adicionartempo(materia,tempodeestudo)
+                adicionarpositionmateria()
                 print("Estudo de",tempodeestudo,"\n")
                 break
+            adicionarposition()
         print("Ciclo finalizado\ndeseja reiniciar o ciclo?")
         resposta = input(":").lower()
         None if resposta=="sim" or resposta=="" else print("saindo...")
