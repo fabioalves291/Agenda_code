ea93d42b6e195f60cfcd295b2ebf3e91c0520ec1
jose fabio <fabioalvesdeandrade29@gmai.com>
2023-02-12 17:24:38 -0300

    autosave

--- controllers/default.py
+++ controllers/default.py
@@ -3,21 +3,15 @@ from tarefas import *
 def lerarquivo(endereço):
     linhaslist= list()
     file = open(endereço,'r',encoding="UTF-8")
-    for linhas in file:
-        fileread = file.readline()
-        linhaslist.append(fileread)
-        print(fileread)
-    
+    linhaslist=file.read().split(";")  
     file.close()
-    return fileread
+    return linhaslist
 
     
 def adicionartempo(materia,tempodeestudo):
     #print(tarefas)
     lista = lerarquivo("controllers/tarefas/tarefas.py")
-    lista = str(lista)
-    lista = lista.split(";")
-    
+    print(lista);input()
     print(materia)
     print(tempodeestudo)
 
--- controllers/tarefas/tarefas.py
+++ controllers/tarefas/tarefas.py
@@ -1,3 +1,4 @@
+
 tarefaconcurso        =   [{"materia":"Língua Portugues","time":0,"position":0},{"materia":"Racocínio Lógico","time":0,"position":0},{"materia":"Informática","time":0,"position":0},{"materia":"Administração","time":0,"position":0},{"mateira":"Noções de contabilidade","time":0,"position":0},{"materia":"Direito Constitucional","time":0,"position":0},{"materia":"Direito Penal","time":0,"position":0},{"materia":"Processual penal","time":0,"position":0},{"materia":"Direito Admnistrativo","time":0,"position":0},{"materia":"Medicina Legal","time":0,"position":0}];
 tarefaescola          =   [{"materia":"materiasdaescola","time":0,"position":1}];
 tarefalivros          =   [{"materia":"informatica","time":0,"position":1}];
