841bddb9a5c6fa1440c7afcd5fe7247cc3c3ac16
jose fabio <fabioalvesdeandrade29@gmai.com>
2023-03-14 11:37:31 -0300

    s

--- run.py
+++ run.py
@@ -1,13 +1,25 @@
 from tarefasfuncao import adicionartempomateria
 from controllers.default import *
-
+import logging
 import os 
-if __name__ == "__main__":
-    while True:
-        input("iniciando...")
-        input("deseja estudar alguma materia especifica?\n>>")
-        print(verificarmaterinotset(),'ciclos finalizados')
-        setarmateriazerada(verificarmaterinotset())
-        adicionartempomateria()
-        input("finalizado:")
+
+try:
+
+    if __name__ == "__main__":
+        while True:
+            input("iniciando...")
+            input("deseja estudar alguma materia especifica?\n>>")
+            print(verificarmaterinotset(),'ciclos finalizados')
+            setarmateriazerada(verificarmaterinotset())
+            adicionartempomateria()
+            input("finalizado:")
+except  Exception as e :
+    print(e)
+    logging.basicConfig(filename="log/log.log",level=logging.DEBUG,filemode="a")
+    logger = logging.getLogger()
+    logger.info(e)
+
+
+    
+
         
