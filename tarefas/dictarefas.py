1104f85059aac7b6a02c6e699e0abfbb13c82439
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2023-08-02 18:00:21 -0300

    autosave

--- tarefas/dictarefas.py
+++ tarefas/dictarefas.py
@@ -104,7 +104,7 @@ def defdictarefas():
             "tarefa0":{
                 "tipo":"materias da escola"     ,
                 "materia":"generica"            ,
-                "time":434562                    ,
+                "time":452565                    ,
                 "position":1                   ,
                 },     
         },
